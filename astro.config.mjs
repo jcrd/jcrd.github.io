5593c427a072a7714947a2ba737686c2a7ce08ee
James Reed <james@twiddlingbits.net>
2023-02-09 20:01:49 -0500

    Add site to config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config"
 
 // https://astro.build/config
-import react from "@astrojs/react";
+import react from "@astrojs/react"
 
 // https://astro.build/config
-import tailwind from "@astrojs/tailwind";
+import tailwind from "@astrojs/tailwind"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
-});
+  site: "https://jcrd.github.io",
+  integrations: [react(), tailwind()],
+})
